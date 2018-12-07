df73d1c454c9d07dc58fc2c2bb4480998b009f86
florianc <castelainflorian44@gmail.com>
2018-12-07 15:04:27 +0100

    read write file, get list of account names

---
+++ app/account_manager.rb
@@ -0,0 +1,19 @@
+class Account_manager
+
+    @@accounts_name
+    @@accounts
+
+    def initialize(data)
+        puts "Initializing accounts"
+        @@accounts = data
+        @@accounts_name = Hash.new(data["acc_qtt"])
+        get_accounts_name(data["accounts"],data["acc_qtt"])
+    end
+
+    def get_accounts_name(accounts, acc_qtt)
+        i = 0
+        for i in 0..acc_qtt-1
+            @@accounts_name[i] = accounts[(i+1).to_s]["name"]
+        end
+    end
+end
